baa79464c446954c98b036c8a4a1e6ae67f8a605
Bradadaniel <danibrada29@gmail.com>
2024-09-15 17:40:36 +0200

    Finished First Project and startring to deploy Tic Tac Toe

---
+++ 02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,38 @@
+export default function GameBoard({ onSelectSquare, board }) {
+  /* const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare();
+  }*/
+
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                {/*<button onClick={() => handleSelectSquare(rowIndex, colIndex)}>*/}
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                  //Ha nem nulla az ertek akkor azt jelenti hogy arra a mezore mar kattintottam
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
---
+++ 02-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+    //setIsEditing(!isEditing);
+    //(isEditing ? false : true) Ezt helyettesiti
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  //let btnCaption = 'Edit';
+
+  if (isEditing === true) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    //btnCaption = 'Save';
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
