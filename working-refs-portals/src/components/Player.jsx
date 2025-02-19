44668a31cf7ad2bffa3fab3b2908b81c75ec44ce
Bradadaniel <danibrada29@gmail.com>
2025-02-19 10:17:29 +0100

    Wokring with Refs

---
+++ working-refs-portals/src/components/Player.jsx
@@ -0,0 +1,49 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+      <p>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
+
+/*
+import { useState } from "react";
+
+export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <p>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}*/
